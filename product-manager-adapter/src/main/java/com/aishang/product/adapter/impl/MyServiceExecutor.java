57ea9fe49a0d597449464f5425d6807032deb07e
yanlijing <000000>
2018-02-07 18:21:11 +0800

    sdasdfasfas

--- product-manager-adapter/src/main/java/com/aishang/product/adapter/impl/MyServiceExecutor.java
+++ product-manager-adapter/src/main/java/com/aishang/product/adapter/impl/MyServiceExecutor.java
@@ -1,59 +1,9 @@
 package com.aishang.product.adapter.impl;
 
-import javax.servlet.http.HttpServletRequest;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-import com.hc.common.logback.SystemCode;
-import com.hc.mvc.core.dto.base.BaseRequestDTO;
-import com.hc.mvc.core.dto.base.BaseResponseDTO;
-import com.hc.mvc.core.dto.base.ResponseCodeEnums;
-import com.hc.mvc.core.service.base.InstantiateUtils;
-import com.hc.mvc.core.service.base.Reflections;
-import com.hc.mvc.core.service.base.ServiceExecutor;
-
-public abstract class MyServiceExecutor<REQUEST extends BaseRequestDTO, RESPONSE extends BaseResponseDTO>
-        extends ServiceExecutor<REQUEST, RESPONSE> {
+public abstract class MyServiceExecutor {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Override
-    protected RESPONSE process(HttpServletRequest request, HttpServletResponse response, REQUEST requestDTO) throws Exception {
-        Class<RESPONSE> responseClass = Reflections.getClassGenricType(this.getClass(), 1);
-        RESPONSE responseDTO = InstantiateUtils.instantiate(responseClass);
-        try {
-            return this.myprocess(request, response, requestDTO);
-        } catch (Exception e) {
-            this.logger.error(SystemCode.EXCEPTION, "process", e);
-            responseDTO.setSuccess(true).setExecuted(false).setResponseCode(ResponseCodeEnums.DATABASE_EXCEPTION)
-                    .setMessage("数据重复");
-            return responseDTO;
-        }
-    }
-
-    @Override
-    protected RESPONSE process(REQUEST requestDTO) throws Exception {
-        Class<RESPONSE> responseClass = Reflections.getClassGenricType(this.getClass(), 1);
-        RESPONSE responseDTO = InstantiateUtils.instantiate(responseClass);
-        try {
-            return this.myprocess(requestDTO);
-        } catch (Exception e) {
-            this.logger.error(SystemCode.EXCEPTION, "process", e);
-            responseDTO.setSuccess(true).setExecuted(false).setResponseCode(ResponseCodeEnums.DATABASE_EXCEPTION)
-                    .setMessage("数据重复");
-            return responseDTO;
-        }
-    }
-
-    protected RESPONSE myprocess(HttpServletRequest request, HttpServletResponse response, REQUEST requestDTO) throws Exception {
-        return null;
-    }
-
-    protected RESPONSE myprocess(REQUEST requestDTO) throws Exception {
-        return null;
-    }
 
 }
--- product-manager-adapter/src/main/java/com/aishang/product/adapter/impl/UserRuleServiceImpl.java
+++ product-manager-adapter/src/main/java/com/aishang/product/adapter/impl/UserRuleServiceImpl.java
@@ -2,22 +2,17 @@ package com.aishang.product.adapter.impl;
 
 
 import com.aishang.product.facade.service.UserRuleService;
-import com.hc.rule.manager.facade.http.SpringSecurityService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 
 @Service
 public class UserRuleServiceImpl<T> implements UserRuleService {
 
-	@Resource
-	private SpringSecurityService springSecurityService;
-	@Value("${rule.manager.system.baseurl}")
-	private String ruleManagerBaseUrl;
-	@Value("${app.code}")
-	private String appCode;
-	
-	
+    @Value("${rule.manager.system.baseurl}")
+    private String ruleManagerBaseUrl;
+    @Value("${app.code}")
+    private String appCode;
+
+
 }
